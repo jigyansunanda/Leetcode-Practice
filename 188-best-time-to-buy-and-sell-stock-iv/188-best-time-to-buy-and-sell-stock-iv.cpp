0d12bf9308ed2c6c8948e0f8c9af66bed667be69
Jigyansu Nanda <62047889+Jigyansu-Nanda@users.noreply.github.com>
2022-08-25 07:31:27 +0530

    Time: 7 ms (91.94%), Space: 11.7 MB (58.39%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-	int dp[1001][101][2];
-
 	int maxProfit(int k, vector<int>& prices) {
-		memset(dp, -1, sizeof dp);
-		return maxProfit(prices, k, 0, 0, 0);
-	}
+		if (k == 0 or prices.size() == 0) return 0;
+		vector<vector<int>> dp(k + 1, vector<int>(prices.size(), 0));
+		vector<int> minBuyingCost(prices.size());
+		for (int transaction = 1; transaction <= k; ++transaction) {
+			for (int day = 0; day < prices.size(); ++day) {
+				if (day == 0) {
+					minBuyingCost[day] = -prices[day];
+					continue;
+				}
 
-	int maxProfit(vector<int> &prices, int k, int index, int hold, int transaction) {
-		if (transaction >= k) return 0;
-		if (index == prices.size()) return 0;
-		if (dp[index][transaction][hold] != -1) return dp[index][transaction][hold];
-		int &ans = dp[index][transaction][hold];
-		if (hold) {
-			int sell = maxProfit(prices, k, index + 1, 0, transaction + 1) + prices[index];
-			int notSell = maxProfit(prices, k, index + 1, 1, transaction);
-			ans = max(sell, notSell);
-		} else {
-			int buy = maxProfit(prices, k, index + 1, 1, transaction) - prices[index];
-			int notBuy = maxProfit(prices, k, index + 1, 0, transaction);
-			ans = max(buy, notBuy);
+				dp[transaction][day] = max(dp[transaction][day], dp[transaction][day - 1]);
+				dp[transaction][day] = max(dp[transaction][day], minBuyingCost[day - 1] + prices[day]);
+				minBuyingCost[day] = max(minBuyingCost[day - 1], dp[transaction - 1][day - 1] - prices[day]);
+			}
 		}
-		return ans;
+		return dp[k][prices.size() - 1];
 	}
 };
