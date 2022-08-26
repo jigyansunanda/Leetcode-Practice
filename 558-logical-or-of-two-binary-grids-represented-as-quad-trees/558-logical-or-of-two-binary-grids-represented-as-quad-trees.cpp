7dbcbbad9ffbdb76c7dc7778abf8320f020b3359
Jigyansu Nanda <62047889+Jigyansu-Nanda@users.noreply.github.com>
2022-08-26 11:09:59 +0530

    Time: 24 ms (83.74%), Space: 15.2 MB (69.92%) - LeetHub

---
+++ 558-logical-or-of-two-binary-grids-represented-as-quad-trees/558-logical-or-of-two-binary-grids-represented-as-quad-trees.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	Node* intersect(Node* quadTree1, Node* quadTree2) {
+		if (quadTree1->isLeaf && quadTree1->val) return quadTree1;
+		if (quadTree2->isLeaf && quadTree2->val) return quadTree2;
+		if (quadTree1->isLeaf && !quadTree1->val) return quadTree2;
+		if (quadTree2->isLeaf && !quadTree2->val) return quadTree1;
+
+		auto tl = intersect(quadTree1->topLeft, quadTree2->topLeft);
+		auto tr = intersect(quadTree1->topRight, quadTree2->topRight);
+		auto bl = intersect(quadTree1->bottomLeft, quadTree2->bottomLeft);
+		auto br = intersect(quadTree1->bottomRight, quadTree2->bottomRight);
+
+		if (tl->val == tr->val && tl->val == bl->val && tl->val == br->val && tl->isLeaf && tr->isLeaf && bl->isLeaf && br->isLeaf)
+			return new Node(tl->val, true, nullptr, nullptr, nullptr, nullptr);
+		else
+			return new Node(false, false, tl, tr, bl, br);
+	}
+};
