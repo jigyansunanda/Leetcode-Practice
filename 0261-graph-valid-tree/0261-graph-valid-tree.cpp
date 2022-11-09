7de67f2755be5b2c7dc22151d8cc8efa90b8d45f
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2022-11-09 19:25:41 +0530

    Time: 45 ms (38.20%), Space: 12 MB (87.29%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -0,0 +1,34 @@
+struct DSU {
+public:
+    int components;
+    vector<int> Parent, Rank;
+
+    DSU(int n) : components(n), Parent(n), Rank(n, 1) { iota(Parent.begin(), Parent.end(), 0); }
+
+    int find(int x) {
+        while (x != Parent[x]) x = Parent[x] = Parent[Parent[x]];
+        return x;
+    }
+
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return false;
+        if (Rank[x] > Rank[y]) swap(x, y);
+        Parent[x] = y;
+        Rank[y] += Rank[x];
+        --components;
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
+        for (auto &e: edges) {
+        	if (dsu.merge(e[0], e[1]) == false) return false;
+        }
+        return (dsu.components == 1);
+    }
+};
