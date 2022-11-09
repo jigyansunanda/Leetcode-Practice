5fa012522af12146ee79d26395d7b26c0dd24b9e
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2022-11-09 19:27:52 +0530

    Time: 7 ms (94.51%), Space: 8.6 MB (96.42%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,34 @@
+struct DSU {
+public:
+    int components;
+    vector<int> Parent, Rank;
+
+    DSU(int n) : components(n), Parent(n), Rank(n, 1) { iota(Parent.begin(), Parent.end(), 0); }
+
+    int find(int x) {
+        while (x != Parent[x]) x = Parent[x] = Parent[Parent[x]];
+        return x;
+    }
+
+    bool merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return false;
+        if (Rank[x] > Rank[y]) swap(x, y);
+        Parent[x] = y;
+        Rank[y] += Rank[x];
+        --components;
+        return true;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        DSU dsu(edges.size() + 1);
+        for (auto &edge : edges) {
+            if (dsu.merge(edge[0], edge[1]) == false) return edge;
+        }
+        return {};
+    }
+};
