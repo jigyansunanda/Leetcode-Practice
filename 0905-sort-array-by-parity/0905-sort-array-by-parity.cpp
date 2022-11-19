ca1b216c603c315115e375d792e82b9c452c392a
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2022-11-19 10:58:24 +0530

    Time: 12 ms (87.31%), Space: 16.2 MB (58.35%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [&](int a, int b) {return a % 2 < b % 2;});
+        return nums;
+    }
+};
