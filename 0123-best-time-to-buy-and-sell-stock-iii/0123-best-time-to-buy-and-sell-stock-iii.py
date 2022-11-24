49d57786898b746c618e785768a192ef94065bde
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2022-11-24 06:12:20 +0530

    Time: 9765 ms (5.11%), Space: 141.3 MB (5.11%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,24 @@
+def solve(index, hold, transaction, prices, dp):
+    if transaction >= 2:
+        return 0
+    if index >= len(prices):
+        return 0
+    if dp[index][hold][transaction] != -1:
+        return dp[index][hold][transaction]
+    dp[index][hold][transaction] = 0
+    if hold == 1:
+        keepHolding = solve(index + 1, hold, transaction, prices, dp)
+        sell = solve(index + 1, 0, transaction + 1, prices, dp) + prices[index]
+        dp[index][hold][transaction] = max(keepHolding, sell)
+    else:
+        buy = solve(index + 1, 1, transaction, prices, dp) - prices[index]
+        dontBuy = solve(index + 1, 0, transaction, prices, dp)
+        dp[index][hold][transaction] = max(buy, dontBuy)
+    return dp[index][hold][transaction]
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        n = len(prices)
+        dp = [[[-1 for _ in range(2)] for _ in range(2)] for _ in range(n)]
+        return solve(0, 0, 0, prices, dp)
