9beccb7e3751bfd6e9814ce6c3909921a71a1495
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2022-12-04 18:20:14 +0530

    Time: 987 ms (52.38%), Space: 122.6 MB (76.19%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,38 +1,28 @@
-struct DSU {
-public:
-    int components;
-    vector<int> Parent, Rank;
-
-    DSU(int n) : components(n), Parent(n), Rank(n, 1) { iota(Parent.begin(), Parent.end(), 0); }
-
-    int find(int x) {
-        while (x != Parent[x]) x = Parent[x] = Parent[Parent[x]];
-        return x;
-    }
-
-    bool merge(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x == y) return false;
-        if (Rank[x] > Rank[y]) swap(x, y);
-        Parent[x] = y;
-        Rank[y] += Rank[x];
-        --components;
-        return true;
-    }
-};
-
 class Solution {
 public:
+    int ans;
+
     int minScore(int n, vector<vector<int>>& roads) {
-        int ans = INT_MAX;
-        DSU dsu(n + 1);
-        for (auto &r : roads) dsu.merge(r[0], r[1]);
+        vector<pair<int, int>> g[n + 1];
         for (auto &r : roads) {
-            if (dsu.find(1) == dsu.find(r[0]) or dsu.find(1) == dsu.find(r[1])) {
-                ans = min(ans, r[2]);
+            g[r[0]].emplace_back(r[1], r[2]);
+            g[r[1]].emplace_back(r[0], r[2]);
+        }
+        ans = 1e7;
+        vector<bool> vis(n + 1, false);
+        dfs(g, 1, vis);
+        return (ans == 1e7) ? -1 : ans;
+    }
+
+    void dfs(vector<pair<int, int>> *g, int u, vector<bool>& vis) {
+        vis[u] = true;
+        for (auto child : g[u]) {
+            int v = child.first;
+            int d = child.second;
+            ans = min(ans, d);
+            if (!vis[v]) {
+                dfs(g, v, vis);
             }
         }
-        return ans;
     }
 };
