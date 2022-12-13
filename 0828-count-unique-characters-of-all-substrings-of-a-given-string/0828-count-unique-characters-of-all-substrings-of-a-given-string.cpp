b2fa629eac90039a55c1faebfb5b6e7307bfc1f8
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2022-12-13 08:19:05 +0530

    Time: 88 ms (69.25%), Space: 11.4 MB (85.88%) - LeetHub

--- 0828-count-unique-characters-of-all-substrings-of-a-given-string/0828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 0828-count-unique-characters-of-all-substrings-of-a-given-string/0828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int uniqueLetterString(string s) {
-        int n = s.size();
-        long long ans = 0;
-        long long lastTwoOcc[26][2];
-        memset(lastTwoOcc, -1, sizeof(long long) * 52);
+        int n = s.size(), ans = 0;
+        int lastTwoOcc[26][2];
+        memset(lastTwoOcc, -1, sizeof(int) * 52);
         for (int i = 0; i < n; ++i) {
             int index = s[i] - 'A';
             ans += (i - lastTwoOcc[index][1]) * (lastTwoOcc[index][1] - lastTwoOcc[index][0]);
