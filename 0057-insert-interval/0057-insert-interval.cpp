31af5cc7f222bbb00a9662136cd34e361a35d12a
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2023-01-16 09:45:33 +0530

    Time: 14 ms (90.83%), Space: 17.1 MB (78.65%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size(), index = 0;
+        vector<vector<int>> ans;
+        while (index < n and intervals[index][1] < newInterval[0]) ans.push_back(intervals[index++]);
+        while (index < n and intervals[index][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[index][0]);
+            newInterval[1] = max(newInterval[1], intervals[index][1]);
+            ++index;
+        }
+        ans.push_back(newInterval);
+        while (index < n) ans.push_back(intervals[index++]);
+        return ans;
+    }
+};
