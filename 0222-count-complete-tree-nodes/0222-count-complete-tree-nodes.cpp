2f82ef29cab37e7bf26ee99a0546566212765f69
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2023-04-02 15:15:11 +0530

    Time: 35 ms (65.56%), Space: 30.8 MB (69.15%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,27 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+private:
+    int get_leftmost_height(TreeNode* root) {
+        int h = 0;
+        while (root) {
+            ++h;
+            root = root->left;
+        }
+        return h;
+    }
+
+    int get_rightmost_height(TreeNode* root) {
+        int h = 0;
+        while (root) {
+            ++h;
+            root = root->right;
+        }
+        return h;
+    }
+
 public:
     int countNodes(TreeNode* root) {
         if (!root) return 0;
-        int leftHeight = 0, rightHeight = 0;
-        TreeNode* curr = root;
-        while (curr) {leftHeight++; curr = curr->left;}
-        curr = root;
-        while (curr) {rightHeight++; curr = curr->right;}
-        if (leftHeight == rightHeight) {
-            return (1 << leftHeight) - 1;
-        } else {
-            return 1 + countNodes(root->left) + countNodes(root->right);
-        }
+        int left_height = get_leftmost_height(root);
+        int right_height = get_rightmost_height(root);
+        if (left_height == right_height) return (1 << left_height) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
