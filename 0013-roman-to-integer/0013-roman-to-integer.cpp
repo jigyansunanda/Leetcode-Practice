74af757dca78b2daf664b2cf4f81b1872d0c4e26
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2023-06-29 18:54:58 +0530

    Time: 20 ms (37.85%), Space: 7.8 MB (53.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    unordered_map<char, int> values = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
     int romanToInt(string s) {
-        int index = 0, ans = 0;
-        while (index < (int)s.size()) {
-            if (index + 1 < (int)s.size() and values[s[index]] < values[s[index + 1]]) {
-                ans += values[s[index + 1]] - values[s[index]];
-                index += 2;
+        unordered_map<char, int> mp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int n = s.size();
+        int ans = 0;
+        for (int i = 0; i < n;) {
+            if (i + 1 < n) {
+                if (mp[s[i]] < mp[s[i + 1]]) {
+                    ans += mp[s[i + 1]] - mp[s[i]];
+                    i += 2;
+                } else {
+                    ans += mp[s[i]];
+                    i += 1;
+                }
             } else {
-                ans += values[s[index]];
-                index += 1;
+                ans += mp[s[i]];
+                i += 1;
             }
         }
         return ans;
