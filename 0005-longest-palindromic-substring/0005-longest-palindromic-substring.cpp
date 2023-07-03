abeb164fac87353ef32fd219cfc1e730b8a5ba3f
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2023-07-03 09:48:09 +0530

    Time: 9 ms (95.66%), Space: 7.6 MB (65.76%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int best_len = 0;
-        string best_s = "";
-        int n = s.length();
-        for(int mid = 0; mid < n; mid++) {
-            for(int x = 0; mid - x >= 0 && mid + x < n; x++) {
-                if(s[mid-x] != s[mid+x]) {
-                    break;
-                }
-                int len = 2 * x + 1;
-                if(len > best_len) {
-                    best_len = len;
-                    best_s = s.substr(mid - x, len);
-                }
+    string longestPalindrome(string S) {
+        int n = S.size();
+        vector<int> d1(n+1, 0), d2(n+1, 0);
+        for (int i = 0, l = 0, r = -1; i < n; i++) {
+            int k = (i > r) ? 1 : min(d1[l + r - i], r - i + 1);
+            while (0 <= i - k && i + k < n && S[i - k] == S[i + k]) {
+                k++;
+            }
+            d1[i] = k--;
+            if (i + k > r) {
+                l = i - k;
+                r = i + k;
             }
         }
-        for(int mid = 0; mid < n - 1; mid++) {
-            for(int x = 1; mid - x + 1 >= 0 && mid + x < n; x++) {
-                if(s[mid-x+1] != s[mid+x]) {
-                    break;
-                }
-                int len = 2 * x;
-                if(len > best_len) {
-                    best_len = len;
-                    best_s = s.substr(mid - x + 1, len);
-                }
+
+        for (int i = 0, l = 0, r = -1; i < n; i++) {
+            int k = (i > r) ? 0 : min(d2[l + r - i + 1], r - i + 1);
+            while (0 <= i - k - 1 && i + k < n && S[i - k - 1] == S[i + k]) {
+                k++;
+            }
+            d2[i] = k--;
+            if (i + k > r) {
+                l = i - k - 1;
+                r = i + k;
             }
         }
-        return best_s;
+
+        int ans = 0, ind = -1;
+        for (int i = 0; i < n; i++) {
+            if (d1[i] * 2 - 1 > ans) ans = d1[i] * 2 - 1, ind = i;
+            if (d2[i] * 2 > ans) ans = d2[i] * 2, ind = i;
+        }
+        return S.substr(ind - ans / 2, ans);
     }
 };
