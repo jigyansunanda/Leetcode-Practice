7d5d4da31135a929441358b2a9281ba8307fec14
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2023-07-15 07:53:25 +0530

    Time: 110 ms (5.66%), Space: 17.3 MB (61.32%) - LeetHub

--- 0749-contain-virus/0749-contain-virus.cpp
+++ 0749-contain-virus/0749-contain-virus.cpp
@@ -1,55 +1,98 @@
 class Solution {
-public:
-    int containVirus(vector<vector<int>>& grid) {
-        while (build_wall(grid)) {}
-        return res;
-    }
-    
 private:
-    int res = 0;
-    int dfs(vector<vector<int>>& grid, int i, int j, int k, int& wall, vector<vector<int>>& infected_grid) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] < 0) return 0;
-        if (grid[i][j] == k) {
-            if (infected_grid[i][j] > 0) wall++; // only build wall on boundary
-            return 0;
+    bool isValid(int row, int col) { return (row >= 0 and row < rows and col >= 0 and col < cols); }
+
+public:
+    vector<vector<int>> grid;
+    int rows, cols;
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+
+    int containVirus(vector<vector<int>>& grid, int walls = 0) {
+        this->grid = grid;
+        this->rows = grid.size();
+        this->cols = grid[0].size();
+        while (true) {
+            int wallsBuild = buildWalls();
+            if (wallsBuild == 0) break;
+            walls += wallsBuild;
         }
-        
-        if (grid[i][j] == 1) {
-            grid[i][j] = k; // grid[i][j] = 1 -> k, not boundary, don't build wall
-            return dfs(grid, i + 1, j, k, wall, infected_grid) + dfs(grid, i, j + 1, k, wall, infected_grid)
-                 + dfs(grid, i - 1, j, k, wall, infected_grid) + dfs(grid, i, j - 1, k, wall, infected_grid);
-        }
-        
-        grid[i][j] = k; // grid[i][j] = 0 -> k
-        infected_grid[i][j]++; // record the boundary
-        wall++; // only build wall on boundary
-        return 1;
+        return walls;
     }
-    
-    bool build_wall(vector<vector<int>>& grid) {
-        int k = 1, m = grid.size(), n = grid[0].size(), max_infected = -1, K = 0, add_wall = 0;
-        vector<vector<int>> infected_grid(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int wall = 0, infected = dfs(grid, i, j, ++k, wall, infected_grid);
-                    if (max_infected < infected) {
-                        max_infected = infected;
-                        add_wall = wall;
-                        K = k;
+
+    int buildWalls() {
+        vector vis(rows, vector<bool>(cols, false));
+        int R = -1, C = -1, maxSpread = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1 and !vis[i][j]) {
+                    int spreadImpact = 0;
+                    vector impact(rows, vector<bool>(cols, false));
+                    checkSpreadImpact(i, j, spreadImpact, vis, impact);
+                    if (maxSpread < spreadImpact) {
+                        maxSpread = spreadImpact;
+                        R = i, C = j;
                     }
                 }
             }
         }
-        
-        if (k == 1) return false; // no free infected area exists, stop
-        
-        res += add_wall;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] > 0) grid[i][j] = (grid[i][j] == K && infected_grid[i][j] <= 1) ? infected_grid[i][j] - 1 : 1; // set area inside new wall as -1, else where as 1
+        if (maxSpread == 0) return 0;
+        int wallCount = 0;
+        buildWallAround(R, C, wallCount);
+        spreadVirus();
+        return wallCount;
+    }
+
+    void checkSpreadImpact(int row, int col, int& spreadImpact, vector<vector<bool>>& vis, vector<vector<bool>>& impact) {
+        if (vis[row][col]) return;
+        vis[row][col] = true;
+        impact[row][col] = true;
+        for (int dir = 0; dir < 4; ++dir) {
+            int x = row + dx[dir];
+            int y = col + dy[dir];
+            if (isValid(x, y)) {
+                if (grid[x][y] == 0) {
+                    if (!impact[x][y]) {
+                        ++spreadImpact;
+                        impact[x][y] = true;
+                    }
+                } else if (grid[x][y] == 1) {
+                    checkSpreadImpact(x, y, spreadImpact, vis, impact);
+                }
             }
         }
-        return true;
+    }
+
+    void buildWallAround(int row, int col, int& wallCount) {
+        grid[row][col] = -1;
+        for (int dir = 0; dir < 4; ++dir) {
+            int x = row + dx[dir];
+            int y = col + dy[dir];
+            if (isValid(x, y)) {
+                if (grid[x][y] == 0) {
+                    ++wallCount;
+                } else if (grid[x][y] == 1) {
+                    buildWallAround(x, y, wallCount);
+                }
+            }
+        }
+    }
+
+    void spreadVirus() {
+        vector spread(rows, vector<bool>(cols, false));
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1 and !spread[i][j]) {
+                    spread[i][j] = true;
+                    for (int dir = 0; dir < 4; ++dir) {
+                        int x = i + dx[dir], y = j + dy[dir];
+                        if (isValid(x, y) and !spread[x][y] and grid[x][y] == 0) {
+                            grid[x][y] = 1;
+                            spread[x][y] = true;
+                        }
+                    }
+                }
+            }
+        }
     }
 };
