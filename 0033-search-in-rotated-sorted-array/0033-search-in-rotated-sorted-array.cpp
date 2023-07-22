0307d64fc6ce0b6c483608e22820a084ff5ba018
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2023-07-22 19:35:52 +0530

    Time: 7 ms (31.45%), Space: 10.9 MB (91.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size(), l = 0, r = n - 1;
+    int search(vector<int>& A, int x) {
+        int l = 0, r = A.size() - 1;
         while (l <= r) {
             int mid = (l + r) >> 1;
-            if (nums[mid] == target) return mid;
-            if (nums[mid] >= nums[l]) {
-                if (nums[l] <= target and target < nums[mid]) r = mid - 1;
+            if (A[mid] == x) return mid;
+            // binary search based on "which side of mid is sorted ? left or right
+            // find out by comparing A[l] and A[mid]"
+            else if (A[l] <= A[mid]) {
+                if (A[l] <= x and x < A[mid]) r = mid - 1;
                 else l = mid + 1;
-            } else {
-                if (nums[mid] < target and target <= nums[r]) l = mid + 1;
+            } else {  // A[l] > A[mid]
+                if (A[mid] < x and x <= A[r]) l = mid + 1;
                 else r = mid - 1;
             }
         }
