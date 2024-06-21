433635cbd20d4200d4918924886d7cfef779039d
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2024-06-21 17:50:33 +0530

    Time: 40 ms (66.39%), Space: 104.6 MB (31.1%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,31 +1,45 @@
-class Solution {
-    private int lengthOfLIS(int[] arr, int n) {
-        int ans = 0;
-        int[] dp = new int[n];
-        for (int x : arr) {
-            int index = Arrays.binarySearch(dp, 0, ans, x);
-            if (index < 0)
-                index = -(index + 1);
-            dp[index] = x;
-            if (index == ans)
-                ++ans;
+class WidthComparator implements Comparator<int[]> {
+    public int compare(final int[] A, final int[] B) {
+        if (A[0] != B[0]) {
+            return A[0] - B[0];
+        } else {
+            return B[1] - A[1];
         }
-        return ans;
+    }
+}
+
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, new WidthComparator());
+        // for (int[] envelope : envelopes) {
+        //     System.out.print("[" + envelope[0] + ", " + envelope[1] + "]  ");
+        // }
+        // System.out.println();
+        int[] tail = new int[envelopes.length];
+        int tailSize = 0;
+        for (int i = 0; i < envelopes.length; i++) {
+            int currentEnvelopeHeight = envelopes[i][1];
+            int index = binarySearchOnHeight(tail, tailSize, currentEnvelopeHeight);
+            if (index + 1 == tailSize) {
+                ++tailSize;
+                tail[index + 1] = currentEnvelopeHeight;
+            } else {
+                tail[index + 1] = Math.min(tail[index + 1], currentEnvelopeHeight);
+            }
+        }
+        return tailSize;
     }
 
-    public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes, new Comparator<int[]>() {
-            public int compare(int[] A, int[] B) {
-                if (A[0] != B[0])
-                    return A[0] - B[0];
-                return B[1] - A[1];
+    private int binarySearchOnHeight(int[] tail, int tailSize, int currentEnvelopeHeight) {
+        int lo = -1, hi = tailSize;
+        while (hi - lo > 1) {
+            int mid = (lo + hi) >> 1;
+            if (tail[mid] < currentEnvelopeHeight) {
+                lo = mid;
+            } else {
+                hi = mid;
             }
-        });
-        int n = envelopes.length;
-        int[] arr = new int[n];
-        for (int i = 0; i < n; ++i) {
-            arr[i] = envelopes[i][1];
         }
-        return lengthOfLIS(arr, n);
+        return lo;
     }
 }
