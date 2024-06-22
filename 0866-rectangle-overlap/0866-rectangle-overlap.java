9f5457ec0a0e24797437ce1c323bb96aaac300f7
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2024-06-22 17:12:40 +0530

    Time: 0 ms (100%), Space: 40.7 MB (61.42%) - LeetHub

--- 0866-rectangle-overlap/0866-rectangle-overlap.java
+++ 0866-rectangle-overlap/0866-rectangle-overlap.java
@@ -1,10 +1,6 @@
 class Solution {
-    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        if(rec1[2] <= rec2[0] ||  //rec2 right 
-           rec2[2] <= rec1[0] ||  // rec2 left
-           rec1[3] <= rec2[1] ||  //rec2 Top
-           rec2[3] <= rec1[1])   //rec2 bottom
-            return false;
+    public boolean isRectangleOverlap(int[] R1, int[] R2) {
+        if (R2[0] >= R1[2] || R1[0] >= R2[2] || R2[1] >= R1[3] || R1[1] >= R2[3]) return false;
         return true;
     }
 }
